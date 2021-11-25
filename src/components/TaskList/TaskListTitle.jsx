bc4a45306dd7b196443b9616bba501458e95cb3f
Lewis Lee <lewisforbusiness@gmail.com>
2021-11-25 15:38:24 -0500

    feat|add expand more icon beside tasklisttitle and add styling to tasklisttitle component

--- src/components/TaskList/TaskListTitle.jsx
+++ src/components/TaskList/TaskListTitle.jsx
@@ -1,17 +1,33 @@
-import { InputBase, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
+import { InputBase, Typography } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { flexbox } from '@mui/system';
 
 const TaskListTitle = props => {
   const [editing, setEditing] = useState(false);
-
+  const useStyle = makeStyles((theme) => ({
+    editableTitleContainer: {
+      marginLeft:theme.spacing(1),
+      display: "flex"
+    },
+    editableTitle: {
+      marginLeft:theme.spacing(1),
+      flexGrow: 1
+    }, 
+  }))
+  const classes = useStyle();
   return (
   <div>
     {editing && <div><InputBase value="ToDo"/></div>}
-    {!editing && <div>
+    {!editing && <div className={classes.editableTitleContainer}>
       <Typography 
-        onClick={() => setEditing(prev => !prev)}>
+        onClick={() => setEditing(prev => !prev)}
+        className={classes.editableTitle}
+        >
         To Do
-        </Typography>
+      </Typography>
+      <ExpandMoreIcon />  
       </div>}
   </div>
   );
