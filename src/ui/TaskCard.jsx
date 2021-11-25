d14dd4dbf4af447a37a6cf365a49ed63ee3cc796
Lewis Lee <lewisforbusiness@gmail.com>
2021-11-25 16:59:37 -0500

    feat|add styling to taskcard ui component

--- src/ui/TaskCard.jsx
+++ src/ui/TaskCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Paper } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyle = makeStyles((theme) => ({
+  taskCard: {
+    padding: theme.spacing(1,1,1,2),
+    margin: theme.spacing(1)
+  }
+}))
 
 const TaskCard = props => {
+  const classes = useStyle(); 
+
   return (
     <div>
-      <Paper>This is Task Card</Paper>
+      <Paper className={classes.taskCard}>This is Task Card</Paper>
     </div>
   )
 }
